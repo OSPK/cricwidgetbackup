bc9069cae34e9808e548f977bc4e86f38dfa7ad6
ospk <waqas@opensource.com.pk>
2016-03-19 18:35:22 +0500

    a

--- code/run.py
+++ code/run.py
@@ -5,8 +5,8 @@ from gevent.wsgi import WSGIServer
 import resource
 resource.setrlimit(resource.RLIMIT_NOFILE, (65536, 65536))
 
-# http_server = WSGIServer(('',8080), app)
-# http_server.serve_forever()
+http_server = WSGIServer(('',8080), app)
+http_server.serve_forever()
 
-app.run(debug=True, port=5000, host='0.0.0.0')
+# app.run(debug=True, port=5000, host='0.0.0.0')
 
