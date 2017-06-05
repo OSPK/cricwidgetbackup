1acad865cf8b3a0712db1989d48ff2e290251145
root <root@score.dailypakistan.com.pk>
2017-06-05 09:04:16 -0400

    a

---
+++ code/test.py
@@ -0,0 +1,12 @@
+#!../.env/bin/python
+#from gevent import monkey; monkey.patch_all()
+from app import app
+#from gevent.wsgi import WSGIServer
+#import resource
+#resource.setrlimit(resource.RLIMIT_NOFILE, (65536, 65536))
+
+#http_server = WSGIServer(('127.0.0.1',8081), app)
+#http_server.serve_forever()
+
+app.run(debug=True, port=5000, host='0.0.0.0')
+
